9776b84364c75c64d7bc88996df9d4cdbb3c3579
bharani <bharani@mocerohealth.in>
2024-07-31 11:21:02 +0530

    vite.config.js  added base: filename

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/EmployeeManagement_File/',
   plugins: [
     vue(),
     vueJsx(),
